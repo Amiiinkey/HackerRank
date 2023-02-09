efd18eda34d9e040d55e86c2d6a39271dedae469
Mohamed <aminito432@gmail.com>
2023-02-09 12:21:42 +0300

    final commit

--- amin.rb
+++ amin.rb
@@ -1,26 +1,52 @@
-
-Mohamed Amin
-3:18 PM
-class Hacker
-end
-class Submission
-end
-class TestCase
-end
-class Contest
-end
 def identify_class(obj)
-case obj
-when Hacker
-  puts "It's a Hacker!"
-when Submission
-  puts "It's a Submission!"
-when TestCase
-  puts "It's a TestCase!"
-when Contest
-  puts "It's a Contest!"
-else
-  puts "It's an unknown model"
+  array = ["Hacker", "Submission", "TestCase", "Content"]
+  case obj
+  when "Hacker"
+    puts "It's a Hacker!"
+  when "Submission"
+    puts "It's a Submission!"
+  when "TestCase"
+    puts "It's a TestCase!"
+  when "Content"
+    puts "It's a Content!"
+  else
+    puts "It's an unknown model"
+  end
 end
-end
-#puts identify_class(Hacker.new)
+
+
+
+
+
+
+
+
+
+
+# For the purpose of printing the output in the terminal
+puts identify_class("Hacker")
+# Mohamed Amin
+# 3:18 PM
+# class Hacker
+# end
+# class Submission
+# end
+# class TestCase
+# end
+# class Contest
+# end
+# def identify_class(obj)
+# case obj
+# when Hacker
+#   puts "It's a Hacker!"
+# when Submission
+#   puts "It's a Submission!"
+# when TestCase
+#   puts "It's a TestCase!"
+# when Contest
+#   puts "It's a Contest!"
+# else
+#   puts "It's an unknown model"
+# end
+# end
+# #puts identify_class(Hacker.new)
